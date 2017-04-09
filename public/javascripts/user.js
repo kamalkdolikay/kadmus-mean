830ac50174c438e9d3b57b4cf000ebd22f1dedf0
Kamal <kamaldolikay@gmail.com>
2017-04-09 15:34:46 +0530

    saving tokens with angular

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -2,6 +2,7 @@ angular
     .module('user', ['ui.router'])
     .controller('main', main)
     .config(config)
+    .factory('auth', auth)
 
 function config($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
@@ -20,6 +21,66 @@ function config($stateProvider, $urlRouterProvider, $locationProvider) {
     $locationProvider.html5Mode(true)
 }
 
-function main($scope, $http) {
+function auth($window, $http) {
+    var auth = {}
+    auth.saveToken = function(token) {
+        $window.localStorage['you-dont-own-me'] = token;
+    };
+
+    auth.getToken = function() {
+        return $window.localStorage['you-dont-own-me'];
+    };
+
+    auth.logIn = function(user) {
+        return $http.post('/login', user).then(function(data) {
+            console.log(data);
+            auth.saveToken(data.data.token);
+        });
+    };
+
+    auth.logInJWT = function(user) {
+        return $http.post('/login-jwt', user).then(function(data) {
+            console.log(data);
+            auth.saveToken(data.data.token);
+        });
+    };
+
+    auth.currentUser = function() {
+        var token = auth.getToken();
+        if (token) {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload;
+        }
+    };
+
+    auth.logOut = function() {
+        $window.localStorage.removeItem('you-dont-own-me');
+    };
+
+    return auth;
+}
+
+function main($scope, $http, auth, $state) {
     $scope.user = "hello";
+    $scope.logOut = auth.logOut;
+    $scope.add = function() {
+        auth.logIn($scope.form).catch(function(error) {
+            console.log("error ", error)
+            $scope.error = error;
+        }).then(function() {
+            $state.go('home1');
+            console.log("getToken", auth.currentUser());
+        });
+    };
+
+    $scope.addjwt = function() {
+        auth.logInJWT($scope.form).catch(function(error) {
+            console.log("error ", error);
+            $scope.error = error;
+        }).then(function() {
+            $state.go('home1');
+            console.log("getToken", auth.currentUser());
+        });
+    };
 }
